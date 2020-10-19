117194fc428554ccb95dab3972cca615f88f7cac
prcptn <f-it@ukr.net>
2020-10-19 18:41:58 +0300

    Minor improvements
    Added androidx implementation, minor layout fixes.

--- Android/Udpsteering/app/src/main/java/com/beamng/remotecontrol/WelcomeActivity.java
+++ Android/Udpsteering/app/src/main/java/com/beamng/remotecontrol/WelcomeActivity.java
@@ -5,10 +5,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class WelcomeActivity extends Activity {
 
@@ -28,7 +29,6 @@ public class WelcomeActivity extends Activity {
                 ActivityCompat.requestPermissions(WelcomeActivity.this,
                     new String[]{Manifest.permission.CAMERA},
                     CamPermission);
-
 
             return;
         }
